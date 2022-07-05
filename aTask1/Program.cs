cf22a959c7b67e92c6ae05f85ec61754983d2762
Fray <kozar20000@mail.ru>
2022-07-05 16:14:47 +0300

    task1

---
+++ aTask1/Program.cs
@@ -0,0 +1,9 @@
+﻿void power (double a, double b)
+{
+    double result = Math.Pow(a, b);
+    Console.WriteLine($"number {a} to the power {b} " + result);
+}
+
+power (3, 5);
+power (2, 4);
+power (5, 7);
