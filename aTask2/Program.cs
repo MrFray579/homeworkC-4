7618ed605daf0628fb3b9a0f91659c692dcee147
Fray <kozar20000@mail.ru>
2022-07-05 18:40:34 +0300

    task2

---
+++ aTask2/Program.cs
@@ -0,0 +1,24 @@
+﻿void num (int a)
+{
+    int result = 0;
+    while (a > 0)
+    {
+        int sum = a % 10;
+        result = result + sum;
+        a = a / 10;
+        
+    }
+    Console.WriteLine(result);
+    
+}
+
+num(568);
+num(57943);
+num(19);
+
+
+
+
+
+
+
