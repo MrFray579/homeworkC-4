8093087d18c5bb26883cc9bdc9b89aa2278ad29d
Fray <kozar20000@mail.ru>
2022-07-05 19:11:32 +0300

    task3

---
+++ aTask3/Program.cs
@@ -0,0 +1,20 @@
+﻿int[] arr (int N, int len)
+{
+    int[] result = new int [N];
+    for (int i = 0; i < len; i++)
+    {
+        int random = new Random().Next(0, N);
+        result[i] = random;
+        Console.Write(result[i] + " ");
+    }
+    Console.WriteLine();
+    return result;
+    
+}
+
+arr(100,5);
+arr(50, 8);
+arr(20, 15);
+
+
+
